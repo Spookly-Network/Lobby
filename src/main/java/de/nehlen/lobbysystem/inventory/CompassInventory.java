208ac1e4f7b546293801ca3bbb18a074ac61c72b
Niklas Ehlen <niklas.ehlen@pixlinemedia.de>
2022-11-20 20:43:32 +0100

    Make position read from config dynamic

--- src/main/java/de/nehlen/lobbysystem/inventory/CompassInventory.java
+++ src/main/java/de/nehlen/lobbysystem/inventory/CompassInventory.java
@@ -35,19 +35,7 @@ public class CompassInventory extends SpigotOnePageInventoryWrapper {
 
     public static void selectPointHandler(Player player, String kitName) {
         Location location = null;
-        if (kitName.equalsIgnoreCase("KnockKnock")) {
-            location = LobbySystem.getLobbySystem().getLocationConfig().getLocation("config.location.KnockKnock");
-        } else if (kitName.equalsIgnoreCase("Bingo")) {
-            location = LobbySystem.getLobbySystem().getLocationConfig().getLocation("config.location.Bingo");
-
-        } else if (kitName.equalsIgnoreCase("Dungeons")) {
-            location = LobbySystem.getLobbySystem().getLocationConfig().getLocation("config.location.Dungeons");
-        } else if (kitName.equalsIgnoreCase("SkyWars")) {
-            location = LobbySystem.getLobbySystem().getLocationConfig().getLocation("config.location.Skywars");
-        } else if (kitName.equalsIgnoreCase("Spawn")) {
-            location = LobbySystem.getLobbySystem().getLocationConfig().getLocation("config.location.Spawn");
-        }
-
+        location = LobbySystem.getLobbySystem().getLocationConfig().getLocation("config.location." + kitName);
         Location finalLocation = location;
         player.teleport(finalLocation);
         Bukkit.getScheduler().runTaskLater(Gameapi.getGameapi(), () -> {
